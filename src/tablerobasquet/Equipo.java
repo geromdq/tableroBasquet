29a7cc67c722adb90e0458ccbe1b356374157acb
Gero <gero295@gmail.com>
2020-02-11 19:22:17 +0300

    Version 2.0

--- src/tablerobasquet/Equipo.java
+++ src/tablerobasquet/Equipo.java
@@ -48,21 +48,47 @@ public class Equipo {
     //Search index player method.
     public int buscarJugador(String jugador) {
         int index = 0;
+        ArrayList<Integer> indexEncontrados = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        
+        //Store all the indexes of the players who matches with String jugador
         for (int i = 0; i < jugadores.size(); i++) {
             if (jugador.equalsIgnoreCase(jugadores.get(i).getNombre())) {
-                index = jugadores.indexOf(jugadores.get(i));
+                indexEncontrados.add(jugadores.indexOf(jugadores.get(i)) + 1);
             }
+        }
+        //If there is more than one player with the same name ask to which one add a stat.
+        if (indexEncontrados.size() > 1) {
+            System.out.println("Nombre repetido, seleccione a que jugador desea sumarle la estadística" + indexEncontrados);
+            String opcion = sc.nextLine();
+            index = (Integer.parseInt(opcion) - 1);
+        //If there is not players with this name.
+        }else if(indexEncontrados.size() == 0) {
+            index = 999; 
+        }
+        //If there is only one player with this name.
+        else{
+            index = indexEncontrados.get(0);
         }
         return index;
     }
 
-    //Add points method.
+    //Receive the index of the player and add points.
     public void meterPuntos(String jugador, int puntos) {
         int index = buscarJugador(jugador);
-        jugadores.get(index).setPuntos(jugadores.get(index).getPuntos() + puntos);
+        if (index == 999)
+        {            
+            System.out.println("No se ha encontrado ese jugador");
+        }
+        else
+        {
+            jugadores.get(index).setPuntos(jugadores.get(index).getPuntos() + puntos);
+        } 
+        
+        
     }
 
-    //Add fouls method.  
+    ///Receive the index of the player and add a foul.
     public void sumarFaltas(String jugador) {
         int index = buscarJugador(jugador);
         jugadores.get(index).setFaltas(jugadores.get(index).getFaltas() + 1);
@@ -74,7 +100,7 @@ public class Equipo {
         int i = 0;
         int puntosTotales = 0;
         while (i < 5) {
-            System.out.println("Los puntos del jugador " + (i + 1) + " del equipo " + numeroEquipo + " son:" + jugadores.get(i).getPuntos() + ", faltas: " + jugadores.get(i).getFaltas());
+            System.out.println("Los puntos del jugador " + jugadores.get(i).getNombre() + " del equipo " + numeroEquipo + " son:" + jugadores.get(i).getPuntos() + ", faltas: " + jugadores.get(i).getFaltas());
             puntosTotales += jugadores.get(i).getPuntos();
             i++;
         }
